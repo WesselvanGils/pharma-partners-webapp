1496ff5349ceddc3270fb32edf4d4a8e09af431e
TheElementalDragon <wesselwammes@hotmail.com>
2021-12-14 10:40:17 +0100

    added some initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
 	declarations: [
-	],
+	
+      LoginComponent,
+      RegisterComponent,
+      HomepageComponent,
+      AboutComponent,
+      SpinnerComponent
+  ],
 	imports: [
 		BrowserModule,
 		AppRoutingModule
