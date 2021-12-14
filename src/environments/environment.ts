d5f582cbedb5d36cb2a6a78ade46b71132062972
TheElementalDragon <wesselwammes@hotmail.com>
2021-12-14 16:43:12 +0100

    patched node in tsconfig.app.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-//const packagejson = require("../../package.json")
+const packagejson = require("../../package.json")
 
 export const environment = 
 {
@@ -10,7 +10,7 @@ export const environment =
 
 	apiUrl: "http://localhost:3000/api/",
 
-	// version: packagejson.version
+	version: packagejson.version
 }
 
 /*
