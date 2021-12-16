9c4a77ff7ad6236d9dc3d13869fb1c2e92e40833
TheElementalDragon <wesselwammes@hotmail.com>
2021-12-16 15:18:18 +0100

    hotfix for heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment =
 {
 	production: true,
 
-	apiUrl: "http://localhost:3000/api/",
+	apiUrl: "https://pharma-partners-api.herokuapp.com/api",
 
 	version: packagejson.version
 }
