4a682301a24431abbc32a7c99ae71859e7c6f7be
TheElementalDragon <wesselwammes@hotmail.com>
2021-12-17 11:55:11 +0100

    updated calendarservice

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,31 +1,31 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { CalendarEvent, CalendarView } from 'angular-calendar';
+import { Observable } from 'rxjs';
+import { Meeting } from 'src/app/models/meeting.model';
 import Swal from 'sweetalert2';
 import { colors } from './calendar-header/colors';
+import { CalendarService } from './calendar.service';
 
 @Component({
 	selector: 'app-calendar',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	templateUrl: './calendar.component.html',
 })
-export class CalendarComponent
+export class CalendarComponent implements OnInit
 {
 	view: CalendarView = CalendarView.Week;
-
 	viewDate: Date = new Date();
+	events: CalendarEvent[] = []
 
-	events: CalendarEvent[] = [
+	constructor (private calendarService: CalendarService) {}
+
+	ngOnInit(): void 
+	{
+		this.calendarService.getAllMeetings().forEach(meeting => 
 		{
-			title: 'Click me',
-			color: colors.yellow,
-			start: new Date(),
-		},
-		{
-			title: 'Or click me',
-			color: colors.blue,
-			start: new Date(),
-		},
-	]
+			this.events.push(meeting.calendarEvent)	
+		})
+	}
 
 	eventClicked({ event }: { event: CalendarEvent }): void
 	{
--- src/app/pages/calendar/calendar.service.ts
+++ src/app/pages/calendar/calendar.service.ts
@@ -3,12 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Meeting } from 'src/app/models/meeting.model';
 import { EntityService } from 'src/app/shared/entity.service';
+import { catchError } from 'rxjs';
+
+const httpOptions = {
+	observe: 'body',
+	responseType: 'json',
+};
 
 @Injectable({ providedIn: 'root' })
 export class CalendarService extends EntityService<Meeting>
 {
-	constructor (http: HttpClient)
-    {
-        super(http, environment.apiUrl, "meetings")
-    }
+	constructor(http: HttpClient)
+	{
+		super(http, environment.apiUrl, "meetings")
+	}
+
+	public getAllMeetings( options?: any ): Meeting[]
+	{
+		let endpoint = `${this.url}${this.endpoint}`;
+		console.log(`list ${endpoint}`);
+		const response = this.http
+			.get<Meeting[]>(endpoint, { ...options, ...httpOptions })
+			.pipe(catchError(this.handleError)
+		)
+		let returnList = []
+		response.subscribe((result) =>
+			result.forEach(item => 
+			{
+				returnList.push(item)
+			}
+		))
+
+		return returnList
+	}
 }
