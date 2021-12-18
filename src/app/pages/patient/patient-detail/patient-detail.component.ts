1a0e2017cb64fc24e79a8b231fa8e782b36b8c7d
dan00n1 <danine.hbo@gmail.com>
2021-12-18 18:02:36 +0100

    detail component layout implementation

--- src/app/pages/patient/patient-detail/patient-detail.component.ts
+++ src/app/pages/patient/patient-detail/patient-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Patient } from 'src/app/models/patient.model';
+import { PatientTempService } from '../patientTemp.service';
 
 @Component({
   selector: 'app-patient-detail',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-detail.component.css']
 })
 export class PatientDetailComponent implements OnInit {
+  patient$!: Observable<Patient>
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private patientService: PatientTempService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.patient$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.patientService.getPatient(params.get('_id')!))
+    )
   }
 
 }
