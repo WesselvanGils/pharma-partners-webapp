9c069adf0b94aaf9997da4f788d738d0f483cb0a
Janne Sterk <jannesterk@hotmail.com>
2021-12-18 10:37:53 +0100

    Added patient list with search function

--- src/app/pages/patient/patient-list/patient-list.component.ts
+++ src/app/pages/patient/patient-list/patient-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of, Subject, switchMap } from 'rxjs';
+import { Patient } from 'src/app/models/patient.model';
+import { PatientService } from '../patient.service';
 
 @Component({
   selector: 'app-patient-list',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientListComponent implements OnInit {
 
-  constructor() { }
+  patients$!: Observable<Patient[] | null>
+  selectedId=0
+
+  patients: Patient[] = [];
+
+  dtOptions: DataTables.Settings = {};
+
+  dtTrigger: Subject<any> = new Subject<any>()
+
+  constructor(
+    private router: Router, 
+    private activatedRoute: ActivatedRoute,
+    private patientService: PatientService
+  ) { }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 2
+    };
+
+
+
+    // this.patients$ = this.activatedRoute.paramMap.pipe(
+    //   switchMap(params => {
+    //     this.selectedId = parseInt(params.get('id')!, 10)
+    //     return this.patientService.list();
+    //   })
+    // )
+
+    this.patientService.list()
+    .subscribe(data => {
+      this.patients = data;
+      // Calling the DT trigger to manually render the table
+      this.dtTrigger.next(this.patients);
+    });
+
   }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+
+
 
 }
